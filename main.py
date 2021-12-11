74ecd725851c7541f568930f51b1f43a6dbf51b1
Canelaguil <canelaguil@gmail.com>
2021-12-12 00:57:32 +0100

    all characters now there

--- main.py
+++ main.py
@@ -60,9 +60,11 @@ class MemoryEvent:
             return "neutral"
         return "not affected"
 
+
 class Character:
     def __init__(self):
-        pass
+        random.seed()
+        self.guilt = 0
 
     def set_up(self):
         # Get traits
@@ -73,7 +75,11 @@ class Character:
 
         # Country affinities
         for country in self.country_affinities:
-            self.country_affinities[country] = random.randint(-3, 3)
+            self.country_affinities[country] += random.randint(-3, 3)
+            if self.country_affinities[country] > 3: 
+                self.country_affinities[country] = 3
+            elif self.country_affinities[country] < -3: 
+                self.country_affinities[country] = -3
 
     def output_child(self):
         self.backstory = self.backstory.replace('\n', '')
@@ -91,7 +97,6 @@ class Character:
 
     def output_adult(self):
         pass
-
 
 
 class Juana(Character):
@@ -145,13 +150,80 @@ class Juana(Character):
         return story
 
 
-    
+class Jules(Character):
+    def __init__(self) -> None:
+        self.name = "Jules"
+        self.surname = "Cohen"
+        self.known_traits = ['impulsive', 'creative']
+        self.my_traits = ['impulsive', 'creative']
+        self.trauma = random.randint(20, 60)
+        self.country_affinities = {
+            'Netherlands': -2, 'Belgium': -1, 'Australia': -1}
+        self.set_up()
+        self.backstory = self.backstory()
+
+    def backstory(self):
+        moveage = random.randint(1, 5)
+        circumstance = random.choice(['thought it would be saver for us', 'got a job', 'liked it'])
+        story = f"I came to the Netherlands when I was {moveage} years old. My father {circumstance} there. "
+        
+        if random.random() < 0.5: 
+            story += "He was a true European. Spoke 4 languages, made himself understood in many more. " + \
+                     "Europe never loved him back enough. Met my mother in a theatre in Brussels, I guess " + \
+                     "they made do well enough. "
+        else:
+            story += "He was born in France, spend time in Berlin, Koningsberg before it got bad. " + \
+                     "He met my mother in Brussels. My mother was an silent movie actress, but after " + \
+                     "the switch to talkies finding work became hard for her in more ways than one. "
+
+        self.age1940 = random.randint(10, 15)
+
+        story += f"When Germany invaded the Netherlands I was {self.age1940}. My parents had send me " + \
+                 " to England days prior. I never saw them again."
+        
+        return story
+
+
+class Ika(Character):
+    def __init__(self) -> None:
+        self.name = "Ika"
+        self.surname = "Nicolaas"
+        self.known_traits = ['brave', 'outgoing']
+        self.my_traits = ['brave', 'outgoing']
+        self.trauma = random.randint(0, 20)
+        self.country_affinities = {
+            'Netherlands': 0, 'Indonesia': 0, 'Australia': 0}
+        self.set_up()
+        self.backstory = self.backstory()
+
+    def backstory(self):
+        self.back_snippets = {
+            'mother' :  "I found out she was murdered for her collaboration with the resistance.", 
+            'father' : "I don't know where my father is now. He was a student in Amsterdam, but he was forced to go " + \
+                       "back to Indonesia when they found out about me."
+        }
+        self.age1940 = random.randint(9, 15)
+        story = "My mother was Dutch, but she was as un-Dutch as one can be. People there love to say " + \
+                "just act normal, that's strange enough, but normal was never strange enough for her. " + \
+                "Just imagine: this good protestant girl from Veenendaal running away to Amsterdam: " + \
+                "she was barely 15. And yet she survived. Became a bartender, took odd jobs wherever she could, " + \
+                "and when my father was forced to go back to Indonesia she raised me on her own. " + \
+                "When the war loomed on the horizon she became too involved in her political causes to look " + \
+                f"after me. I was {self.age1940} when she send me away."
+      
+        return story
 
 
 class Controller:
-    def __init__(self, input_file='') -> None:
-        j = Juana()
-        j.output_child()
+    def __init__(self) -> None:
+        self.cs = {
+            'Juana' : Juana(),
+            'Jules' : Jules(),
+            'Ika' : Ika()
+        }
+
+        for child in self.cs.values(): 
+            child.output_child()
 
 Controller()
 
